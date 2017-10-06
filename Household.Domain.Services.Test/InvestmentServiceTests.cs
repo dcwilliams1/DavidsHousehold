159b9f798f60ee376c0db0a19af7ebf7a27fadc9
David <David@openroadsoftware.com>
2017-10-05 22:37:35 -0600

    Added first two Investment Repository tests

--- Household.Domain.Services.Test/InvestmentServiceTests.cs
+++ Household.Domain.Services.Test/InvestmentServiceTests.cs
@@ -20,52 +20,52 @@ namespace Household.Domain.Services.Test
         int investmentFundId;
         int accountId;
         Investment testInvestmnt;
+        IInvestmentRepository repository;
+        InvestmentService svc;
 
         [TestInitialize]
         public void TestSetup()
         {
             mock = new MockRepository();
+            repository = mock.StrictMock<IInvestmentRepository>();
+            testInvestmnt = new Investment(accountId, investmentFundId);
+            svc = new InvestmentService(repository);
             transactionAmt = 10.5M;
             investmentFundId = 2;
             accountId = 3;
-            testInvestmnt = new Investment(accountId, investmentFundId);
         }
 
-        //[TestMethod]
-        //public void UpdatingPrincipleCallsRepository()
-        //{
-        //    var initialBalance = testInvestmnt.Balance;
+        [TestMethod]
+        public void UpdatingPrincipleCallsRepository()
+        {
+            var initialBalance = testInvestmnt.Balance;
+            repository.Expect(rep => rep.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt)).Return(initialBalance + transactionAmt);
+            repository.Replay();
 
-        //    IInvestmentRepository repository = mock.StrictMock<IInvestmentRepository>();
-        //    //repository.Stub(rep => rep.UpdateInvestmentPrinciple(Arg<Investment>.Is.Anything, Arg<decimal>.Is.Anything));
-        //    repository.Expect(rep => rep.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt)).Return(initialBalance + transactionAmt);
-        //    var svc = new InvestmentService(repository);
-        //    svc.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt);
+            svc.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt);
 
-        //    //repository.AssertWasCalled(rep => rep.UpdateInvestmentPrinciple(Arg<Investment>.Is.Anything, Arg<decimal>.Is.Anything));
-        //    //repository.AssertWasCalled(rep => rep.UpdateInvestmentPrinciple(Arg<Investment>.Is.Equal(testInvestmnt), Arg<decimal>.Is.Equal(transactionAmt)));
-        //    repository.VerifyAllExpectations();
-        //}
+            repository.AssertWasCalled(rep => rep.UpdateInvestmentPrinciple(Arg<Investment>.Is.Anything, Arg<decimal>.Is.Anything));
+        }
 
+        [TestMethod]
+        public void UpdatingPrincipleReturnsInvestmentBalance()
+        {
+            var initialBalance = testInvestmnt.Balance;
+            repository.Expect(rep => rep.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt)).Return(initialBalance + transactionAmt);
+            repository.Replay();
 
+            var actualBalance = svc.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt);
+            var expectedBalance = initialBalance + transactionAmt;
+            
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
 
-        //[TestMethod]
-        //public void UpdatingPrincipleReturnsInvestmentBalance()
-        //{
-
-        //    var testInvestmnt = new Investment(accountId, investmentFundId);
-        //    var initialBalance = testInvestmnt.Balance;
-
-        //    IInvestmentRepository repository = mock.StrictMock<IInvestmentRepository>();
-        //    repository.Stub(rep => rep.UpdateInvestmentPrinciple(Arg<Investment>.Is.Anything, Arg<decimal>.Is.Anything));
-
-        //    var svc = new InvestmentService(repository);
-        //    svc.UpdateInvestmentPrinciple(testInvestmnt, transactionAmt);
-
-        //    var expectedBalance = initialBalance + transactionAmt;
-
-        //    Assert.AreEqual(expectedBalance, testInvestmnt.Balance);
-        //}
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            repository.VerifyAllExpectations();
+            
+        }
 
 
     }
